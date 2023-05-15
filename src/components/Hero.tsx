fa6970cd2fc8f9e16953657c54ac13a370140435
Qmun14 <mamunramdhan@gmail.com>
2023-05-16 00:01:50 +0700

    Building the Hero Section (2/2)

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -2,6 +2,10 @@
 
 import { Cursor, useTypewriter } from "react-simple-typewriter"
 import BackgroundCircles from "./BackgroundCircles"
+import Image from "next/image"
+import Link from "next/link";
+
+import image from '../../public/me/qmun.jpg';
 
 type Props = {}
 
@@ -14,10 +18,28 @@ export default function Hero({ }: Props) {
   return (
     <div className="h-screen flex flex-col space-y-8 items-center justify-center text-center overflow-hidden">
       <BackgroundCircles />
-      <h1>
-        <span>{text}</span>
-        <Cursor cursorColor="#00ff00" />
-      </h1>
+      <Image src={image} alt={"qmun foto"} height={150} width={150} className="relative rounded-full mx-auto object-cover" />
+      <div className="z-20">
+        <h2 className="text-sm uppercase text-[#335c85] pb-2 tracking-[15px] ">Software Engineer</h2>
+        <h1 className="text-5xl lg:text-6xl font-semibold px-10">
+          <span className="mr-3">{text}</span>
+          <Cursor cursorColor="#00ff00" />
+        </h1>
+        <div className="pt-5">
+          <Link href="#about">
+            <button className="heroButton">About</button>
+          </Link>
+          <Link href="#experience">
+            <button className="heroButton">Experience</button>
+          </Link>
+          <Link href="#skills">
+            <button className="heroButton">Skills</button>
+          </Link>
+          <Link href="#projects">
+            <button className="heroButton">Projects</button>
+          </Link>
+        </div>
+      </div>
     </div>
   )
 }
