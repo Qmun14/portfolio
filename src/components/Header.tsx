95b8eee880bcfa94c4dac4a2ed15f6eedd2123c6
Qmun14 <mamunramdhan@gmail.com>
2023-05-15 21:22:15 +0700

    Implements framer motion to our Header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,11 +1,28 @@
-import { SocialIcon } from "react-social-icons"
+"use client"
+
+import { motion } from "framer-motion";
+import { SocialIcon } from "react-social-icons";
 
 type Props = {}
 
 export default function Header({ }: Props) {
   return (
     <header className="sticky top-0 mx-auto p-5 flex items-start justify-between max-w-7xl z-20 xl:items-center">
-      <div className="flex flex-row items-center">
+      <motion.div
+        initial={{
+          x: -500,
+          opacity: 0,
+          scale: 0.5
+        }}
+        animate={{
+          x: 0,
+          opacity: 1,
+          scale: 1
+        }}
+        transition={{
+          duration: 1.5,
+        }}
+        className="flex flex-row items-center">
         {/* Social Icons */}
         <SocialIcon
           url="https://twitter.com/mr_dh4n"
@@ -22,8 +39,20 @@ export default function Header({ }: Props) {
           fgColor="gray"
           bgColor="transparent"
         />
-      </div>
-      <div className="flex flex-row items-center text-gray-300 cursor-pointer">
+      </motion.div>
+      <motion.div
+        initial={{
+          x: 500,
+          opacity: 0,
+          scale: 0.5
+        }}
+        animate={{
+          x: 0,
+          opacity: 1,
+          scale: 1
+        }}
+        transition={{ duration: 1.5 }}
+        className="flex flex-row items-center text-gray-300 cursor-pointer">
         <SocialIcon
           className="cursor-pointer"
           network="email"
@@ -31,7 +60,7 @@ export default function Header({ }: Props) {
           bgColor="transparent"
         />
         <p className="uppercase hidden md:inline-flex text-sm text-gray-400">Get In Touch</p>
-      </div>
+      </motion.div>
     </header>
   )
 }
