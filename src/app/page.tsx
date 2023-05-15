2e6be39ab82a31eef6396bafc7ed619a8ebbe300
Qmun14 <mamunramdhan@gmail.com>
2023-05-15 22:42:29 +0700

    Building the Hero section, Implements react-simple-typewriter, adding background ciccles w/ framer motion

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,17 @@
 import Header from "@/components/Header";
+import Hero from "@/components/Hero";
 
 export default function Home() {
   return (
-    <div>
+    <div className="bg-[rgb(15,23,42)] text-[#B4C6EF] h-screen snap-y snap-mandatory overflow-scroll z-0">
       {/* Header */}
       <Header />
+
+      {/* Hero */}
+      <section id="hero" className="snap-center">
+        <Hero />
+      </section>
+
     </div>
   )
 }
