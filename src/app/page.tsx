a056998073fea7c1bc897dc3682dfabb002d8adb
Qmun14 <mamunramdhan@gmail.com>
2023-05-19 18:08:57 +0700

    Adding the Home Button

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,6 +4,9 @@ import Header from "@/components/Header";
 import Hero from "@/components/Hero";
 import Projects from "@/components/Projects";
 import Skills from "@/components/Skills";
+import Image from "next/image";
+import Link from "next/link";
+import image from '../../public/me/ocie.jpg'
 
 export default function Home() {
   return (
@@ -34,6 +37,14 @@ export default function Home() {
       <section id="contact" className="snap-start">
         <ContactMe />
       </section>
+
+      <Link href="#hero">
+        <footer className="sticky bottom-3 w-full cursor-pointer">
+          <div className="flex items-center justify-center">
+            <Image src={image.src} alt="qmun14-cat" className="h-10 w-10 rounded-full filter grayscale hover:grayscale-0 cursor-pointer" width={10} height={10} />
+          </div>
+        </footer>
+      </Link>
 
     </div>
   )
